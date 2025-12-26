0d0febd8a605d4dd0b35c28ee1c59277e9d32b14
Subhojit <rabinath750@gmail.com>
2025-12-26 22:19:47 +0530

    adding range min query

--- Segment Trees/lazyPropagation.cpp
+++ Segment Trees/lazyPropagation.cpp
@@ -93,6 +93,99 @@ public:
         return left + right;
     }
 };
+class SegmentTreeMin
+{
+public:
+    vector<int> seg, lazy;
+    SegmentTreeMin(int n)
+    {
+        seg.resize(4 * n + 1);
+        lazy.resize(4 * n + 1, 0);
+    }
+    void build(int ind, int low, int high, int arr[])
+    {
+        if (low == high)
+        {
+            seg[ind] = arr[low];
+            return;
+        }
+
+        int mid = (low + high) / 2;
+        build(2 * ind + 1, low, mid, arr);
+        build(2 * ind + 2, mid + 1, high, arr);
+        seg[ind] = min(seg[2 * ind + 1] , seg[2 * ind + 2]);
+    }
+
+    void update(int ind, int low, int high, int l, int r, int val)
+    {
+        // update the previous remaining updates and propagate downwards
+        if (lazy[ind] != 0)
+        {
+            seg[ind] += lazy[ind];
+            // propagate downwards
+            if (low != high)
+            {
+                lazy[2 * ind + 1] += lazy[ind];
+                lazy[2 * ind + 2] += lazy[ind];
+                lazy[ind] = 0;
+            }
+        }
+
+        // no overlap
+        // we dont do anything and return
+        if (high < l || r < low)
+            return;
+
+        // complete overlap
+        if (low >= l && high <= r)
+        {
+            seg[ind] += val;
+            if (low != high)
+            {
+                lazy[2 * ind + 1] += val;
+                lazy[2 * ind + 2] += val;
+            }
+            return;
+        }
+
+        // partial overlap
+        int mid = (low + high) / 2;
+        update(2 * ind + 1, low, mid, l, r, val);
+        update(2 * ind + 2, mid + 1, high, l, r, val);
+
+        seg[ind] = min(seg[2 * ind + 1] , seg[2 * ind + 2]);
+    }
+
+    int query(int ind, int low, int high, int l, int r)
+    {
+        // update the previous remaining updates and propagate downwards
+        if (lazy[ind] != 0)
+        {
+            seg[ind] += lazy[ind];
+            // propagate downwards
+            if (low != high)
+            {
+                lazy[2 * ind + 1] += lazy[ind];
+                lazy[2 * ind + 2] += lazy[ind];
+                lazy[ind] = 0;
+            }
+        }
+
+        // no overlap
+        if (high < l || r < low)
+            return INT_MAX;
+        // complete overlap
+        if (low >= l && high <= r)
+        {
+            return seg[ind];
+        }
+        // partial overlap
+        int mid = (low + high) / 2;
+        int left = query(2 * ind + 1, low, mid, l, r);
+        int right = query(2 * ind + 2, mid + 1, high, l, r);
+        return min(left , right);
+    }
+};
 void solve()
 {
     int n;
